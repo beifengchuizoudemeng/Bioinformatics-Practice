4387f7ae6b8505d133300449b16a82f7095b6ce9
beifengchuizoudemeng <1542072481@qq.com>
2019-01-02 14:45:03 +0800

    删除多余代码

--- project/SVM_mode.py
+++ project/SVM_mode.py
@@ -2,7 +2,6 @@ from sklearn.svm import SVC
 import numpy as np
 
 def classifyBySVM(Train_donors, Train_labels, Test_donors1, Test_labels1, Test_donors2, Test_labels2):
-    import numpy as np
     X_train = np.array(Train_donors)
     y_train = np.array(Train_labels)
     X_test1 = np.array(Test_donors1)
